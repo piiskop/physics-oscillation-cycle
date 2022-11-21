bb6533056b8611fa6699085e9904f20dbf991585
peacecop kalmer <kalmer@test.tennis24.ee>
2022-11-21 13:40:21 +0200

    initial

--- js.js
+++ js.js
@@ -38,9 +38,9 @@ function setTargets() {
   document.getElementById("book-source").target = "GitHub";
   
   let div = document.getElementById("main-nav").getElementsByTagName("nav")[0].getElementsByClassName("book-extra")[0];
-  createLink(div, "book-print", "en.pdf", "View printable book");
-  let nameOfPresentation = "presentation";
-  createLink(div, "presentation-" + nameOfPresentation, nameOfPresentation + ".html", "Presentation");
+  createLink(div, "book-print", "physics-oscillation-cycle.pdf", "View printable book");
+  // let nameOfPresentation = "presentation";
+  // createLink(div, "presentation-" + nameOfPresentation, nameOfPresentation + ".html", "Presentation");
 }
 
 window.onload = function () {
